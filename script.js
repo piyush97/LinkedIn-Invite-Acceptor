0e2ed18f77231cda8a5f70e04a8d1602e2de4664
Piyush Mehta <me@piyushmehta.com>
2019-03-15 23:53:30 +0530

    Loop to check

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-var x = document.getElementsByClassName('button-secoondary-medium')
+var x = document.getElementsByClassName('button-secondary-medium')
+for (var i = 0; i < x.length ; i++ ) {
+    console.log('LCO')
+}
